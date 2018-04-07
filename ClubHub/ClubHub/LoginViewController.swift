dfb13f648fdb253abf0a4e9b1df437e3cb9a9505
ChrisGiuffrida <chris@gonotredame.net>
2018-04-07 16:41:33 -0400

    Google Sign In

---
+++ ClubHub/ClubHub/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  ClubHub
+//
+//  Created by Christopher Giuffrida on 4/7/18.
+//  Copyright © 2018 Christopher Giuffrida. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class LoginViewController: UIViewController, GIDSignInUIDelegate {
+    
+    @IBOutlet weak var GIDSignInButton: GIDSignInButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        
+        // TODO(developer) Configure the sign-in button look/feel
+        // ...
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
